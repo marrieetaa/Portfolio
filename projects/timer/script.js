9eb9227e550e42999c9aac6dac5d14980dfd2994
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-05 21:19:27 +0400

    Create `startTimer` function for `stopwatch`

--- projects/timer/script.js
+++ projects/timer/script.js
@@ -2,3 +2,31 @@
 let hours = 0, minutes = 0, seconds = 0, milliseconds = 0;
 let timer;
 let running = false;
+
+//Timer start function
+function startTimer() {
+
+    if(!running){
+        running = true;
+
+        timer = setInterval(() => {
+        milliseconds += 10;
+        if(milliseconds === 1000) {
+            milliseconds = 0;
+            seconds++;
+        }
+        if(seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+        if(minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+      },10)  
+    }
+}
+
+//
+const start = document.getElementById('start');
+start.addEventListener('click',startTimer);
