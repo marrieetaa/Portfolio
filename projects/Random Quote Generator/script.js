c403737e266b135e1c8b8cd2b73a8459e8f4e955
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-06 21:39:51 +0400

    Created the `quotes` array in the JS file

---
+++ projects/Random Quote Generator/script.js
@@ -0,0 +1,9 @@
+//Quotes array
+const quotes = [
+  'Believe you can and you\'re halfway there.',
+  'Character is power.',
+  'Never be limited by other people\'s limited imaginations.',
+  'Life is what happens when you\'re busy making other plans.',
+  'In the end, we will remember not the words of our enemies, but the silence of our friends.',
+  'We cannot solve problems with the kind of thinking we employed when we came up with them.\n -Albert Einstein'
+]
