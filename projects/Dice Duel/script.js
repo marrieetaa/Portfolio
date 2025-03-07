c02d46f9d4b048002c4b476ec052da1090209272
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-07 23:05:05 +0400

    Replaced the entry with backticks and added alt attribute content change based on the display number

--- projects/Dice Duel/script.js
+++ projects/Dice Duel/script.js
@@ -7,8 +7,10 @@ let randomNum1 = randomNumber();
 let randomNum2 = randomNumber(); 
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
-image1.src = 'dicePictures/dice' + randomNum1 + '.png';
-image2.src = 'dicePictures/dice' + randomNum2 + '.png';
+image1.src = `dicePictures/dice${randomNum1}.png`;
+image2.src = `dicePictures/dice${randomNum2}.png`;
+image1.alt = `Dice showing ${randomNum1}`;
+image2.alt = `Dice showing ${randomNum2}`;
 }
 
 //
