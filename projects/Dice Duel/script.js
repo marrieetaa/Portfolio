9f5bca035691bbf51a75a2dcaeae24219263be04
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-07 22:36:18 +0400

    Created `randomNumber` function and stored the variables

--- projects/Dice Duel/script.js
+++ projects/Dice Duel/script.js
@@ -1,6 +1,10 @@
-let randomNum1 = Math.floor(Math.random() * 6) + 1;
-let randomNum2 = Math.floor(Math.random() * 6) + 1; 
+//Random Number Function
+const randomNumber = () => Math.floor(Math.random() * 6 ) + 1;
+//
+let randomNum1 = randomNumber();
+let randomNum2 = randomNumber(); 
 
+//
 const image1 = document.getElementById('image1');
 image1.src = 'dicePictures/dice' + randomNum1 + '.png';
 const image2 = document.getElementById('image2');
