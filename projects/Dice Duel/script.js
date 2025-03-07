68dc0c36c92e652ffbd4c8fcc2ad23e603c2818e
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-07 22:23:17 +0400

    Created image elments in HTML and used JS to display a corresponding image based on a random number

--- projects/Dice Duel/script.js
+++ projects/Dice Duel/script.js
@@ -1,2 +1,10 @@
 let randomNum1 = Math.floor(Math.random() * 6) + 1;
 let randomNum2 = Math.floor(Math.random() * 6) + 1; 
+
+const image1 = document.getElementById('image1');
+image1.src = 'dicePictures/dice' + randomNum1 + '.png';
+const image2 = document.getElementById('image2');
+image2.src = 'dicePictures/dice' + randomNum2 + '.png';
+
+
+
