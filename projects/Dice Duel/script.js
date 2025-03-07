b164d8f40a32a91c8de14c224f322617c59369ed
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-07 23:22:40 +0400

    Create `result` function to display result on the screen

--- projects/Dice Duel/script.js
+++ projects/Dice Duel/script.js
@@ -11,9 +11,24 @@ image1.src = `dicePictures/dice${randomNum1}.png`;
 image2.src = `dicePictures/dice${randomNum2}.png`;
 image1.alt = `Dice showing ${randomNum1}`;
 image2.alt = `Dice showing ${randomNum2}`;
+
+//Display the result on the screen
+const resultText = document.getElementById('result-text');
+const result = () => {
+   if(randomNum1 > randomNum2) {
+     return resultText.innerText = 'Player one won';
+   }else if (randomNum2 > randomNum1) {
+    return resultText.innerText = 'Player two won'
+   }else{
+    return resultText.innerText = 'Tie';
+   }
+}
+result()
 }
 
 //
 const diceButton = document.getElementById('dice-button');
 diceButton.addEventListener('click',rollDice);
+
+
 
