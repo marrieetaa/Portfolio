30c710a84fb09a91743b521daa348c3de9be4e81
Mariam Diasamidze <diasamidzemariam2101@gmail.com>
2025-03-07 22:51:11 +0400

    Create the `rollDice`function that combines random number generation and excute it on button click

--- projects/Dice Duel/script.js
+++ projects/Dice Duel/script.js
@@ -1,14 +1,17 @@
 //Random Number Function
 const randomNumber = () => Math.floor(Math.random() * 6 ) + 1;
-//
+
+//Roll Dice function
+const rollDice = () => {
 let randomNum1 = randomNumber();
 let randomNum2 = randomNumber(); 
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+image1.src = 'dicePictures/dice' + randomNum1 + '.png';
+image2.src = 'dicePictures/dice' + randomNum2 + '.png';
+}
 
 //
-const image1 = document.getElementById('image1');
-image1.src = 'dicePictures/dice' + randomNum1 + '.png';
-const image2 = document.getElementById('image2');
-image2.src = 'dicePictures/dice' + randomNum2 + '.png';
-
-
+const diceButton = document.getElementById('dice-button');
+diceButton.addEventListener('click',rollDice);
 
